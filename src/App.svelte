400d0425923a1b8254cbccbe41d019a111c26375
Sebastian Gnat <upgradesebastian@gmail.com>
2022-09-21 04:04:34 +0200

    not ready preview

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,22 @@
   let content = 'This is sample content'
 </script>
 
-<textarea name="" id="" cols="30" rows="10" bind:value={content} />
+<style>
+  #not-ready-yet {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+  }
+</style>
+
+<!-- <textarea name="" id="" cols="30" rows="10" bind:value={content} /> -->
+
+<div id="not-ready-yet">
+  <h1>📘 PowerNotes</h1>
+  <h2>app coming soon...</h2>
+
+</div>
